341061567482c1e134f00f74263450084a41ebb2
momobo <massimo.morelli@gmail.com>
2014-06-21 18:20:35 +0200

    added a functioning plot

--- save/ui.R
+++ save/ui.R
@@ -1,28 +1,28 @@
 library(shiny)
 
-# Define UI
+# Define UI 
 shinyUI(
   pageWithSidebar(
     # Application title
     headerPanel("Car gasoline consume prediction"),
-    
+  
     sidebarPanel(
       numericInput('weight', 'Weight kg', 1000, min = 500, max = 4000, step = 5),
-      # numericInput('am', 'Transmission (0: man, 1: aut)', 0, min = 0, max = 1, step = 1),
+     # numericInput('am', 'Transmission (0: man, 1: aut)', 0, min = 0, max = 1, step = 1),
       radioButtons("am", label = "Select Transmission",
                    choices = list("Manual" = 1, "Automatic" = 2),selected = 1),
       numericInput('qsec', '1/4 mile  (400m) time (s)', 20, min = 4, max = 50, step = 1)
-      #, submitButton('Submit')
+      , submitButton('Submit')
     ),
     mainPanel(
-      h3('Results of prediction'),
-      h4('You entered'),
-      verbatimTextOutput("inputValue"),
-      h4('Consume predicted in km/l: '),
-      verbatimTextOutput("prediction"),
-      
-      plotOutput("distPlot")
-      
+        h3('Results of prediction'),
+        h4('You entered'),
+        verbatimTextOutput("inputValue"),
+        h4('Consume predicted in km/l: '),
+        verbatimTextOutput("prediction"),
+
+        plotOutput("distPlot")
+
     )
   )
 )
